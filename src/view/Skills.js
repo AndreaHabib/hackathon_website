a56eccb8bf5969295eb004a94c6c86c380445381
AndreaHabib <andrea.atef@yahoo.com>
2021-07-29 15:19:51 -0400

    FEAT: improved skills images

--- src/view/Skills.js
+++ src/view/Skills.js
@@ -6,16 +6,19 @@ export default function Skills() {
       <h1 className="title">SHOW OFF YOU SKILLS!</h1>
       <div id="section3_container">
         <img
+          style={{ boxShadow: "3px 3px rgba(25, 37, 99, 0.9)" }}
           className="skillsIMG"
           src="https://res.cloudinary.com/andreahabib/image/upload/v1618146852/hackathon/day95-app-development_1_1_luk4mb.png"
           alt="appdev"
         />
         <img
+          style={{ boxShadow: "3px 3px rgba(25, 37, 99, 0.9)" }}
           className="skillsIMG"
           src="https://res.cloudinary.com/andreahabib/image/upload/v1618146714/hackathon/day93-programing_1_ufurus.png"
           alt="programming"
         />
         <img
+          style={{ boxShadow: "3px 3px rgba(25, 37, 99, 0.9)" }}
           className="skillsIMG"
           src="https://res.cloudinary.com/andreahabib/image/upload/v1618146714/hackathon/day94-ui-ux_1_tkb1te.png"
           alt="ui/ux"
